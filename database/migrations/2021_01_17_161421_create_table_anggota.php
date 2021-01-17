0766db213f1239ff7677bf7bc21cbb981735b233
Theo Pires <teo.mateus97@gmail.com>
2021-01-18 00:28:02 +0800

    first commit

---
+++ database/migrations/2021_01_17_161421_create_table_anggota.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableAnggota extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+Schema::create('table_anggota', function (Blueprint $table) {
+
+    $table->bigIncrements('id_anggota');
+    $table->string('nama_anggota',50);
+    $table->string('jenis_kelamin',50);
+    $table->string('alamat');
+    $table->string('email',50);
+    $table->string('no_telp',20);
+    $table->timestamps();
+    });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_anggota');
+    }
+}
