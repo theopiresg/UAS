4bb84548b710fde15bfa8cfaa93f64aaadd8b9e2
Theo Pires <teo.mateus97@gmail.com>
2021-01-18 19:52:30 +0800

    Membuat Main View

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main')
+@section('title', 'Laravel - SI Perpustakaan')
+@section('content')
+
+    <div class="container">
+    <div class="jumbotron">
+    <CENTER><h1 class="display-4">KONOHA LIBRARY</h1></CENTER>
+
+    <hr class="my-4">  
+
+
+    </div>
+    </div>
+
+@endsection
